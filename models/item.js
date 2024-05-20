c1ac3f061a84be08692407be5b464c5a37e0b176
Dave Shevitz <dave@modular.com>
2024-05-19 20:40:57 -0700

    remove placeholder code

--- models/item.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const itemSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  price: { type: Number, required: true },
-});
-
-module.exports = mongoose.model("items", itemSchema);
